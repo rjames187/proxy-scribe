ea4cd85242431b5800e3afdcbb0acc28c5fff60a
rjames187 <rory.james2021@gmail.com>
2023-09-02 18:41:59 -0500

    create basic proxy for get requests only

--- main.go
+++ main.go
@@ -1,1 +1,12 @@
 package main
+
+import (
+	"log"
+	"net/http"
+	"proxy-scribe/handler"
+)
+
+func main() {
+	log.Print("Proxy is listening")
+	log.Fatal(http.ListenAndServe(":4000", &handler.ProxyHandler{}))
+}
