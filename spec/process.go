7f4e0c39899e79e454cbdd42b83462e659df167f
rjames187 <rory.james2021@gmail.com>
2023-09-10 15:43:45 -0500

    write func for converting body to openapi

---
+++ spec/process.go
@@ -0,0 +1,51 @@
+package spec
+
+import (
+	"fmt"
+	"reflect"
+)
+
+func buildPrimitive(kind string, example interface{}) map[string]interface{} {
+	result := make(map[string]interface{})
+	result["type"] = kind
+	if example != nil {
+		result["example"] = example
+	}
+	return result
+}
+
+func convertBody(data map[string]interface{}) map[string]interface{} {
+	result := make(map[string]interface{})
+
+	for key, value := range data {
+		switch v := value.(type) {
+		case string:
+			result[key] = buildPrimitive("string", value)
+		case int:
+			result[key] = buildPrimitive("int", value)
+		case bool:
+			result[key] = buildPrimitive("bool", value)
+		case []interface{}:
+			array := make(map[string]interface{})
+			array["type"] = "array"
+			if len(v) > 0 {
+				item := v[0]
+				if _, ok := item.(map[string]interface{}); ok {
+					array["items"] = convertBody(item.(map[string]interface{}))
+				} else {
+					itemType := reflect.TypeOf(item)
+					array["items"] = buildPrimitive(itemType.String(), item)
+				}
+			}
+			result[key] = array
+		case map[string]interface{}:
+			result[key] = convertBody(v)
+		default:
+			fmt.Print("Unkown type detected: ")
+			fmt.Print(value)
+			result[key] = value
+		}
+	}
+
+	return result
+}
