12917ecc7cc3db384cfb38385067d79319546bb1
rjames187 <rory.james2021@gmail.com>
2023-09-12 10:24:28 -0500

    create repl to enable multiple paths

--- main.go
+++ main.go
@@ -1,13 +1,35 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"proxy-scribe/handler"
+	"strings"
 )
 
 func main() {
-	log.Print("Proxy is listening")
-	controller := &handler.ProxyHandler{}
-	log.Fatal(http.ListenAndServe(":4000", controller))
+	reader := bufio.NewScanner(os.Stdin)
+	fmt.Print("ProxyScribe > ")
+	for reader.Scan() {
+		input := parseInput(reader.Text())
+		if len(input) < 1 {
+			continue
+		}
+		switch input[0] {
+		case "record":
+			log.Print("Proxy is listening")
+			controller := &handler.ProxyHandler{}
+			log.Fatal(http.ListenAndServe(":4000", controller))
+		}
+		fmt.Print("ProxyScribe > ")
+	}
+}
+
+func parseInput(input string) []string {
+	input = strings.TrimSpace(input)
+	input = strings.ToLower(input)
+	return strings.Split(input, " ")
 }
